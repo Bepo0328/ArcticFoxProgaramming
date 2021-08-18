fd95cbf570ea9f994156aa675b0dbef5076ae09f
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-08-18 21:31:31 +0900

    Chapter 43: Jetpack 데이터 바인딩 예제 프로젝트

--- Ch40 & Ch41 & Ch43/src/main/java/kr/co/bepo/arcticfoxprogaramming/ui/main/MainFragment.kt
+++ Ch40 & Ch41 & Ch43/src/main/java/kr/co/bepo/arcticfoxprogaramming/ui/main/MainFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import kr.co.bepo.arcticfoxprogaramming.BR.myViewModel
+import kr.co.bepo.arcticfoxprogaramming.R
 import kr.co.bepo.arcticfoxprogaramming.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
@@ -15,35 +17,25 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private var _binding: MainFragmentBinding? = null
-    private val binding get() = _binding!!
+    lateinit var binding: MainFragmentBinding
 
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = MainFragmentBinding.inflate(inflater, container, false)
-        .also { _binding = it }
-        .root
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        val resultObserver = Observer<Float> { result ->
-            binding.resultText.text = result.toString()
-        }
-
-        viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
-
-        binding.convertButton.setOnClickListener {
-            if (binding.dollarText.text.isNotEmpty()) {
-                viewModel.setAmount(binding.dollarText.text.toString())
-            } else {
-                binding.resultText.text = "No Value"
-            }
-        }
+        binding.setVariable(myViewModel, viewModel)
     }
 
 }
